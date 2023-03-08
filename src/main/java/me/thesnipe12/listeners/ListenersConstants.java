4726500f8d72b70a639cf28194db369afd7f45f0
Yishai Levy <96019039+thesnipe12@users.noreply.github.com>
2023-03-08 12:59:05 +0200

    Used `PLUGIN_LINK` instead of raw text

--- src/main/java/me/thesnipe12/listeners/ListenersConstants.java
+++ src/main/java/me/thesnipe12/listeners/ListenersConstants.java
@@ -1,5 +1,6 @@
 package me.thesnipe12.listeners;
 
+import me.thesnipe12.PluginConstants;
 import net.md_5.bungee.api.chat.ClickEvent;
 import net.md_5.bungee.api.chat.HoverEvent;
 import net.md_5.bungee.api.chat.TextComponent;
@@ -10,16 +11,14 @@ public class ListenersConstants {
     static final TextComponent
             PREFIX = new TextComponent(ChatColor.GREEN + "MCLimiter" + ChatColor.GRAY + " >>" + ChatColor.RESET + " "),
             NOT_UP_TO_DATE_MESSAGE_PART1 = new TextComponent("There is a new version of the plugin available! Go to "),
-            NOT_UP_TO_DATE_MESSAGE_PART2 = new TextComponent("\"https://www.spigotmc.org/resources/simplecl.101603/\""),
+            NOT_UP_TO_DATE_MESSAGE_PART2 = new TextComponent("\""+ PluginConstants.PLUGIN_LINK +"\""),
             NOT_UP_TO_DATE_MESSAGE_PART3 = new TextComponent(" to download it!");
 
     static {
         NOT_UP_TO_DATE_MESSAGE_PART1.setColor(net.md_5.bungee.api.ChatColor.RED);
         NOT_UP_TO_DATE_MESSAGE_PART2.setColor(net.md_5.bungee.api.ChatColor.GOLD);
-        NOT_UP_TO_DATE_MESSAGE_PART2.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL,
-                "https://www.spigotmc.org/resources/simplecl.101603/"));
-        NOT_UP_TO_DATE_MESSAGE_PART2.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT,
-                new Text("Click to open URL")));
+        NOT_UP_TO_DATE_MESSAGE_PART2.setClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, PluginConstants.PLUGIN_LINK));
+        NOT_UP_TO_DATE_MESSAGE_PART2.setHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, new Text("Click to open the link!")));
         NOT_UP_TO_DATE_MESSAGE_PART3.setColor(net.md_5.bungee.api.ChatColor.RED);
     }
 }
